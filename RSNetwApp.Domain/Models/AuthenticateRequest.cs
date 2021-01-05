db511924a10414f7bf9333c5ce68152e9a9e5398
roman <roman@DESKTOP-VUEJ976>
2021-01-05 12:07:45 +0200

    AuthenticateRequest was changed.

--- RSNetwApp.Domain/Models/AuthenticateRequest.cs
+++ RSNetwApp.Domain/Models/AuthenticateRequest.cs
@@ -2,7 +2,7 @@
 {
     public class AuthenticateRequest
     {
-        public string Login { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
