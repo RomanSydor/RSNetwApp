3d784e3d963aad42fc0507a2a9d23a9d2cfe8213
roman <roman@DESKTOP-VUEJ976>
2021-02-08 11:58:03 +0200

    Modified swagger.

--- RSNetwApp.Api/Startup.cs
+++ RSNetwApp.Api/Startup.cs
@@ -39,14 +39,40 @@ namespace RSNetwApp.Api
                                      options.UseSqlServer(
                                          Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(swagger =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo
+                //This is to generate the Default UI of Swagger Documentation
+                swagger.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
                     Title = "RSNetw Api"
                 });
+                // To Enable authorization using Swagger (JWT)
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
+
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
