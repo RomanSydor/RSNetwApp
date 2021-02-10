43a407a1500ff94faf4de8711501c5f75a0f457b
roman <roman@DESKTOP-VUEJ976>
2021-02-10 10:42:44 +0200

    Added errors handling.

--- RSNetwApp.Api/Controllers/UserProfileController.cs
+++ RSNetwApp.Api/Controllers/UserProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RSNetwApp.Domain;
 using RSNetwApp.Domain.Entities;
 using RSNetwApp.Services.Interfaces;
 using System.Linq;
@@ -25,6 +27,8 @@ namespace RSNetwApp.Api.Controllers
         public async Task<IActionResult> GetUserProfiles()
         {
             var profiles = await _service.GetUserProfileEntitiesAsync();
+            if (profiles == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Something went wrong" });
             return Ok(profiles);
         }
 
@@ -34,6 +38,8 @@ namespace RSNetwApp.Api.Controllers
         {
             var username = User.Identity.Name;
             var profile = await _service.GetUserProfileAsync(username);
+            if (profile == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Something went wrong" });
             return Ok(profile);
         }
 
@@ -42,6 +48,8 @@ namespace RSNetwApp.Api.Controllers
         public async Task<IActionResult> ProfileDetails(string username)
         {
             var profile = await _service.GetUserProfileAsync(username);
+            if (profile == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Something went wrond" });
             return Ok(profile);
         }
     }
